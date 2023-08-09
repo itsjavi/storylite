639e8c1b4d0604fde6f16360f9ae4e6a6679de47
Javi Aguilar <122741+itsjavi@users.noreply.github.com>
2023-08-09 20:55:12 +0200

    chore: use jest preset from r1stack

--- jest.config.js
+++ jest.config.js
@@ -2,31 +2,6 @@
  * @type {import('@jest/types').Config.InitialOptions}
  */
 module.exports = {
-  coverageProvider: 'v8',
-  collectCoverageFrom: ['packages/*/src/**/*.{ts,tsx}'],
-  projects: [
-    {
-      displayName: 'DOM',
-      coveragePathIgnorePatterns: ['types.ts', 'index.ts', 'index.tsx', '/node_modules/'],
-      testEnvironment: './jest.env-browser.ts',
-      testMatch: ['**/*.test.ts', '**/*.test.tsx'],
-      testPathIgnorePatterns: ['/node_modules/', '/.local/', '/.ignore/', '/dist/'],
-      // setupFilesAfterEnv: ['./jest.setup-browser.js'],
-      transform: {
-        '^.+\\.(t|j)sx?$': ['@swc/jest'],
-      },
-      transformIgnorePatterns: ['/node_modules/', '^.+\\.module\\.(css|sass|scss)$'],
-    },
-    {
-      displayName: 'NodeJS',
-      coveragePathIgnorePatterns: ['types.ts', 'index.ts', '/node_modules/'],
-      testEnvironment: 'node',
-      testMatch: ['**/*.test.ts'],
-      testPathIgnorePatterns: ['/node_modules/', '/.local/', '/.ignore/', '/dist/', '.dom.test.ts'],
-      transform: {
-        '^.+\\.(t|j)s$': ['@swc/jest'],
-      },
-      transformIgnorePatterns: ['/node_modules/', '^.+\\.module\\.(css|sass|scss)$'],
-    },
-  ],
+  preset: '@r1stack/coding-style/jest-preset',
+  // other options and overrides
 }
